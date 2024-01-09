bfc52789d518670d487ef7af5c0a65072e76f6ce
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-01-09 16:36:01 +0300

    fixed task addition bug, design issues

--- components/TaskLogic.tsx
+++ components/TaskLogic.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import TaskInput from "./TaskInput";
 import TaskList from "./TaskList";
+import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { View, StyleSheet } from "react-native";
