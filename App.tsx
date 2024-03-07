5ea749283b0736da87a5041d9e31e6132a0c1278
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-03-08 00:01:03 +0300

    implemented different screens

--- App.tsx
+++ App.tsx
@@ -3,25 +3,36 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import MainScreen from "./components/helpers/MainScreen";
 import Footer from "./components/Footer";
+import SettingsScreen from "./components/SettingsScreen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import TaskScreen from "./components/TaskScreen";
 import TimerScreen from "./components/TimerScreen";
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    // we'll most likely handle screen switching here
     <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <View style={styles.container}>
-          <StatusBar style="auto" />
-          {/* <MainScreen /> */}
-          <TimerScreen />
-          <TaskScreen />
-          <Footer />
-        </View>
+        <NavigationContainer>
+          <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Stack.Navigator initialRouteName="TimerScreen">
+              {/* <MainScreen /> */}
+              <Stack.Screen name="TimerScreen" component={TimerScreen} />
+              <Stack.Screen name="TaskScreen" component={TaskScreen} />
+              <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
+            </Stack.Navigator>
+            <Footer
+              screenNames={["TimerScreen", "TaskScreen", "SettingsScreen"]}
+            />
+          </View>
+        </NavigationContainer>
       </GestureHandlerRootView>
     </SafeAreaProvider>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,24 @@
-import { View, Text, Linking, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import { ReactElement, JSXElementConstructor, ReactNode, Key } from "react";
 // this will change into a screen switcher, timer-tasks-settings
 // if i do implement a stats screen, it will be under settings
 
-export default function Footer() {
+export default function Footer({ screenNames }: { screenNames: string[] }) {
+  const navigation = useNavigation();
   return (
     <View style={styles.footer}>
-      <TouchableOpacity onPress={() => Linking.openURL("timer")}>
-        {/* the linking part may be wrong, change this later */}
-        <Text style={styles.footerText}>Timer</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => Linking.openURL("tasks")}>
-        <Text style={styles.footerText}>Tasks</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => Linking.openURL("settings")}>
-        <Text style={styles.footerText}>Settings</Text>
-      </TouchableOpacity>
+      {screenNames.map((screenName: string) => (
+        <TouchableOpacity
+          key={screenName}
+          onPress={() => navigation.navigate(screenName)}
+          // this gives an error but it works, at least on the web
+          // check again on mobile
+        >
+          <Text style={styles.footerText}>{screenName}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
